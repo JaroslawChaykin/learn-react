b75d0a75e59fd6a02d645ceb873c6f0d6b7cfb7c
Jaroslaw <jaroslaw.chaikin@gmail.com>
2021-08-09 18:18:29 +0300

    first commit

---
+++ src/components/Profile.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./Profile.module.css"
+
+const Profile = () => {
+    return (
+        <div className={'content'}>
+            <div>
+                <img src="" alt=""/>
+            </div>
+            <div>
+                ava - description
+            </div>
+            <div>
+                new posts
+            </div>
+            <div>Posts
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+                <div>
+                    post 3
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
